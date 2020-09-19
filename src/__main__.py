935af7d48e0a5800d2f5318989174f1ec649a98f
Josh Thorpe <josh@thorpe.engineering>
2020-09-20 02:12:55 +1000

    Split project up into seperate modules

---
+++ src/__main__.py
@@ -0,0 +1,23 @@
+""" Image sorting tool module. This script launches a tkinter GUI that allows images to be sorted
+based on their date taken.
+"""
+import logging
+from gui import GUI
+
+
+# Create root logger
+LOG_FORMAT = '%(levelname)s %(asctime)s : %(message)s'
+logger = logging.getLogger('root')
+logger.setLevel(logging.DEBUG)
+
+# Log to stdout
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(logging.Formatter(LOG_FORMAT))
+stream_handler.setLevel(logging.DEBUG)
+logger.addHandler(stream_handler)
+
+if __name__ == "__main__":
+    logger.info('Entered Image Sorting Tool')
+    root = GUI()
+    root.draw_main()
+    root.mainloop()
