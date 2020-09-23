d17a612a0a9db2f6c56f4820fb783401eac46a8d
Josh Thorpe <josh@thorpe.engineering>
2020-09-23 14:32:07 +1000

    Test for the sorting functionality

--- image_sorting_tool/tests/test_image_sort.py
+++ image_sorting_tool/tests/test_image_sort.py
@@ -36,3 +36,45 @@ def test_find_images(tmp_path):
     assert all([src == found for src, found in zip(src_assets, sorter.image_list)])
     # Cleanup child threads
     sorter.cleanup()
+
+
+def test_sort_images(tmp_path):
+    """ Test that the tool can sort the images in the test assets directory
+    """
+    sorted_gt = ['failed_to_sort/no_exif.jpg',
+                 '2013/04/20130407_132135.JPG',
+                 '2013/04/20130408_131738.JPG']
+    # Convert posix path to string for older python versions
+    tmp_path = str(tmp_path)
+
+    # Create tmp directories for the test
+    tmp_src = os.path.join(tmp_path, 'src/')
+    tmp_dst = os.path.join(tmp_path, 'dst/')
+    os.mkdir(tmp_src)
+    os.mkdir(tmp_dst)
+
+    # Add the tmp directory to the ground truths
+    sorted_gt = [os.path.join(tmp_dst, gt_path) for gt_path in sorted_gt]
+
+    # Copy test assets to a tmp_path
+    for asset in TEST_ASSETS:
+        shutil.copy2(asset, tmp_src)
+    sorter = ImageSort(tmp_src, tmp_dst, None)
+    sorter.find_images()
+
+    # Run the sorting
+    sorter.run_parallel_sorting()
+
+    # Find sorted images
+    sorted_list = []
+    for root_path, __, files in os.walk(tmp_dst):
+        for file_name in files:
+            sorted_list.append(os.path.join(root_path, file_name))
+
+    # Check sorted images where sorted correctly
+    sorted_list.sort()
+    sorted_gt.sort()
+    assert len(sorted_list) == len(sorted_gt)
+    assert all([sort_path == gt_path for sort_path, gt_path in zip(sorted_list, sorted_gt)])
+    # Cleanup child threads
+    sorter.cleanup()
